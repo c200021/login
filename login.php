8ca0650c61a1ea16d5f42303994d8fee80aaa987
c200021 <c200021@ccg.ac.jp>
2022-01-09 09:33:49 +0000

    first commit

--- login.php
+++ login.php
@@ -21,11 +21,11 @@ if (!empty($_POST)) {
             $stmt->execute();
 			$member = $stmt->fetch(PDO::FETCH_ASSOC);
 //  ここにパスワードのチェック処理を完成させる
-//  		if( xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ) {
+            if(password_verify($password, $member['password'])) {
 				// ログイン成功
-
+                
 //  ここにセッションハイジャック対策を追加
-//
+//              session_regenetate_id(true);
 
 				$_SESSION['id'] = $member['id'];
 				$_SESSION['time'] = time();
